adc6d113aa7ee9db681682543f2021cbfb406454
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-06-24 18:13:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+    def bubble_sort(liste):
+        for i in range(len(liste) - 1, 0, -1):
+            for j in range(i):
+                n_courant = liste[j]
+                n_suivant = liste[j + 1]
+                if n_courant > n_suivant:
+                    liste[j] = n_suivant
+                    liste[j + 1] = n_courant
+     
+        return liste
+     
+    l = [40, 16, 80, 2, 90]
+    print(bubble_sort(l))
